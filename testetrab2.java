690a960613927b5dbe6eb2c3da670fbf230b0678
Blacklinkker <55569386+Blacklinkker@users.noreply.github.com>
2019-10-28 14:21:17 -0300

    Update testetrab2.java

--- testetrab2.java
+++ testetrab2.java
@@ -5,6 +5,43 @@ public class testetrab2{
 	static int operacao, i, j, conta,contarico,troca,a,fim;
 	static double saldo, transacao,maior,aux;
 	
+	static String A[][]=new String[3][3];
+	static{
+		A[0][0] = "Tibúrcio";
+		A[0][1] = "Ricardo";
+		A[0][2] = "João";
+		A[1][0] = "Felisbino";
+		A[1][1] = "Seu Zé";
+		A[1][2] = "Dona Maria";
+		A[2][0] = "Gedonstrôncio";
+		A[2][1] = "Batman";
+		A[2][2] = "Homem de Ferro";
+	}
+	static int B[][]=new int[3][3];
+	static{
+		B[0][0] = 111;
+		B[0][1] = 222;
+		B[0][2] = 333;
+		B[1][0] = 444;
+		B[1][1] = 555;
+		B[1][2] = 666;
+		B[2][0] = 777;
+		B[2][1] = 888;
+		B[2][2] = 999;
+	}
+	static double C[][]=new double[3][3];
+	static{
+		C[0][0]=0.0;
+		C[0][1]=0.0;
+		C[0][2]=0.0;
+		C[1][0]=0.0;
+		C[1][1]=0.0;
+		C[1][2]=0.0;
+		C[2][0]=0.0;
+		C[2][1]=0.0;
+		C[2][2]=0.0;
+	}
+	
 	//Inserir Recebimento
 	public static double Recebimento(int conta,double saldo){
 		int i,j;
@@ -46,7 +83,7 @@ public class testetrab2{
 			}
 		}
 	}
-	
+
 
 	public static void main(String[] args) {
 		

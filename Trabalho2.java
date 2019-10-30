0d3fee6fcc3e27e70ce45328e079b54f959145fc
Blacklinkker <55569386+Blacklinkker@users.noreply.github.com>
2019-10-30 10:58:31 -0300

    Update Trabalho2.java

--- Trabalho2.java
+++ Trabalho2.java
@@ -1,35 +1,13 @@
 import javax.swing.JOptionPane;
 import java.util.*;
-public class Trabalho2{
+public class testetrab2{
+	static String nome,todosnomes,todascontas,todossaldos,rico,nome1;
+	static int operacao, i, j, conta,contarico,troca,a,fim;
+	static double saldo, transacao,maior,aux;
+	static double vetorsaldo[]=new double[9];
 	
-	//Inserir Recebimento
-	public static double Recebimento(int conta,double saldo){
-		int i,j;
-		for (i=0;i<3 ;i++ ) {
-			for (j=0;j<3 ;j++ ) {
-				if (B[i][j] == conta) {
-					C[i][j] = C[i][j] + transacao;
-					saldo = C[i][j];
-					nome = A[i][j];
-					return saldo;
-				}
-			}
-		}
-	}
-	public static void main(String[] args) {
-		String nome,todosnomes,todascontas,todossaldos,rico;
-		int operacao, i, j, conta,contarico,troca,a,fim;
-		double saldo, transacao,maior,aux;
-		
-		Matriz matrizes=new Matriz[3][3];
-		for (i=0;i<3;i++ ) {
-			for (j=0;j<3 ;j++ ) {
-				matrizes[i][j]=new Matriz();
-			}
-		}
-
-
-		String A[][]=new String[3][3];
+	public static final String A[][]=new String[3][3];
+	static{
 		A[0][0] = "Tibúrcio";
 		A[0][1] = "Ricardo";
 		A[0][2] = "João";
@@ -39,8 +17,10 @@ public class Trabalho2{
 		A[2][0] = "Gedonstrôncio";
 		A[2][1] = "Batman";
 		A[2][2] = "Homem de Ferro";
-		
-		int B[][]=new int[3][3];
+	}
+	
+	static int B[][]=new int[3][3];
+	static{
 		B[0][0] = 111;
 		B[0][1] = 222;
 		B[0][2] = 333;
@@ -50,8 +30,10 @@ public class Trabalho2{
 		B[2][0] = 777;
 		B[2][1] = 888;
 		B[2][2] = 999;
-
-		double C[][]=new double[3][3];
+	}
+	
+	static double C[][]=new double[3][3];
+	static{
 		C[0][0]=0.0;
 		C[0][1]=0.0;
 		C[0][2]=0.0;
@@ -61,7 +43,95 @@ public class Trabalho2{
 		C[2][0]=0.0;
 		C[2][1]=0.0;
 		C[2][2]=0.0;
-		
+	}
+	
+	//1 – Inserir Recebimento 
+	public static double Recebimento(int conta1,double transacao1){
+		int i,j;
+		Double saldo1=0.0;
+		for (i=0;i<3 ;i++ ) {
+			for (j=0;j<3 ;j++ ) {
+				if (B[i][j] == conta1) {
+					C[i][j] = C[i][j] + transacao1;
+					saldo1 = C[i][j];
+					nome1= A[i][j];
+				}
+			}
+		}return saldo1;
+	}
+
+	//2 – Inserir Pagamento
+	public static double Pagamento(int conta,double pagamento){
+		for (i=0; i<3; i++) {
+			for (j=0; j<3; j++) {
+				if (B[i][j] == conta) {
+					C[i][j] = C[i][j] - transacao;
+					saldo = C[i][j];
+					nome = A[i][j];
+					operacao=0;
+					return saldo;
+				}
+			}
+		}return saldo;
+	}
+	//3 – Mostre em apenas uma mensagem todos os Números de Conta, Correntistas e Saldos respectivos
+	public static void TodosNomesContasSaldos(){
+		todosnomes="";
+		todossaldos="";
+		todascontas="";
+		for (i=0;i<3 ;i++ ) {
+			for (j=0;j<3 ;j++ ) {
+				todosnomes=todosnomes + " : "+A[i][j];
+				todascontas=todascontas+" : "+B[i][j];
+				todossaldos=todossaldos+" : "+C[i][j];
+				operacao = 0;
+			}
+		}
+	}
+
+	//4 – Mostre em apenas uma mensagem o Número da Conta, o Nome do Correntista e o saldo do correntista mais RICO.
+	public static int Maisrico(){
+		contarico=0;
+		maior=0;
+		rico="";
+		for (i=0;i<3 ;i++ ) {
+			for (j=0;j<3;j++) {
+				if (C[i][j]>maior) {
+					maior=C[i][j];
+					rico=A[i][j];
+					contarico=B[i][j];
+					operacao = 0;
+				}
+			}
+		}return contarico;
+	}
+
+	//5 – Mostre em apenas uma mensagem todos os saldos ordenados do menor para o maior usando BubbleSort.
+
+	public static void OrdenadoMaisrico(){
+		a=0;
+		for (i=0;i<3 ;i++ ) {
+			for (j=0;j<3 ;j++ ) {
+				vetorsaldo[a]=C[i][j];
+				a++;
+			}
+		}
+		troca=1;
+		fim=a-1;
+		while(troca==1){
+			troca = 0;
+			for(i=0; i<fim;i++){
+				if (vetorsaldo[i] > vetorsaldo[i+1]){
+					aux = vetorsaldo[i];
+					vetorsaldo[i] = vetorsaldo[i+1];
+					vetorsaldo[i+1]=aux;
+					troca = 1;
+				}	
+			}
+		}                    
+	}
+
+	public static void main(String[] args) {
 		operacao = 0;
 		
 		while(operacao<=5){
@@ -79,88 +149,34 @@ public class Trabalho2{
 				case 1:
 					conta = Integer.parseInt(JOptionPane.showInputDialog("Insira o numero da conta"));
 					transacao = Double.parseDouble(JOptionPane.showInputDialog("Insira o valor a ser depositado"));
-					Recebimento(conta,transacao);
-					JOptionPane.showMessageDialog(null,nome + " seu saldo atual é de: " + saldo);
+					saldo=Recebimento(conta,transacao);
+					JOptionPane.showMessageDialog(null,nome1 + " seu saldo atual é de: " + saldo);
 					operacao = 0;
 				break;
 			
-				//Inserir Pagamento 
 				case 2:
 					conta = Integer.parseInt(JOptionPane.showInputDialog("Insira o número da conta"));
 					transacao = Double.parseDouble(JOptionPane.showInputDialog("Insira o valor do pagamento"));
-					for (i=0; i<3; i++) {
-						for (j=0; j<3; j++) {
-							if (B[i][j] == conta) {
-								C[i][j] = C[i][j] - transacao;
-								saldo = C[i][j];
-								nome = A[i][j];
-								JOptionPane.showMessageDialog(null, nome + " seu saldo atual é de: " + saldo);
-								operacao=0;
-							}
-						}
-					}
+					Pagamento(conta,transacao);
+					JOptionPane.showMessageDialog(null, nome + " seu saldo atual é de: " + saldo);
 				break;
 				
-				//Números de Conta, Correntistas e Saldos respectivos
 				case 3:
-					todosnomes="";
-					todossaldos="";
-					todascontas="";
-					for (i=0;i<3 ;i++ ) {
-						for (j=0;j<3 ;j++ ) {
-							todosnomes=todosnomes + " : "+A[i][j];
-							todascontas=todascontas+" : "+B[i][j];
-							todossaldos=todossaldos+" : "+C[i][j];
-							operacao = 0;
-						}
-					}
+					TodosNomesContasSaldos();
 					JOptionPane.showMessageDialog(null,"Números das contas " + todascontas
 						+"\nNomes dos correntistas " + todosnomes
 						+"\nSaldos das contas " + todossaldos);
 				break;
-	
-				// o Número da Conta, o Nome do Correntista e o saldo do correntista mais RICO
+
 				case 4:
-					contarico=0;
-					maior=0;
-					rico="";
-					for (i=0;i<3 ;i++ ) {
-						for (j=0;j<3;j++) {
-							if (C[i][j]>maior) {
-								maior=C[i][j];
-								rico=A[i][j];
-								contarico=B[i][j];
-								operacao = 0;
-							}
-						}
-					}JOptionPane.showMessageDialog(null," Numero da conta do correntista mais rico: "+contarico
+					contarico=Maisrico();
+					JOptionPane.showMessageDialog(null," Numero da conta do correntista mais rico: "+contarico
 						+"\n Em nome de: "+rico
 						+"\n Com o saldo de: "+maior);
 				break;
 
-				// todos os saldos ordenados do menor para o maior usando BubbleSort.
-				case 5:
-					a=0;
-					double vetorsaldo[]=new double[9];
-					for (i=0;i<3 ;i++ ) {
-						for (j=0;j<3 ;j++ ) {
-							vetorsaldo[a]=C[i][j];
-							a++;
-						}
-					}
-					troca=1;
-					fim=a-1;
-					while(troca==1){
-						troca = 0;
-						for(i=0; i<fim;i++){
-							if (vetorsaldo[i] > vetorsaldo[i+1]){
-								aux = vetorsaldo[i];
-								vetorsaldo[i] = vetorsaldo[i+1];
-								vetorsaldo[i+1]=aux;
-								troca = 1;
-							}	
-						}
-					}                                             
+				case 5:                   
+					OrdenadoMaisrico();
 					JOptionPane.showMessageDialog(null,"Os saldos ordenados do menor para o maior são: \n"+ Arrays.toString(vetorsaldo));
 					operacao = 0;
 				break;
